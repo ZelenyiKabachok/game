363da8e2c0782750e94d17084c08b7aa2e2574e9
Alexsey <Lex.B13@protonmail.com>
2021-10-19 21:23:13 +1000

    Correct

--- src/gravity_object.cpp
+++ src/gravity_object.cpp
@@ -3,12 +3,13 @@
 void GravityObject::Attract(float delta_time)
 {
 	//float attract = ObSpeed.y/delta_time;
-	float ForceOfAttract = mass * 9.8;
-	float ResistanceForce = (-ObSpeed.y) * cooficient;
-	float FinalForce = ForceOfAttract - ResistanceForce;
-	float boost = FinalForce/mass;
-	ObSpeed.y += (-boost) * delta_time;
+	vec3 ForceOfAttract;
+	ForceOfAttract.y = mass * 9.8;
+	vec3 ResistanceForce = (-ObSpeed) * cooficient;
+	vec3 FinalForce = ForceOfAttract - ResistanceForce;
+	vec3 boost = FinalForce/mass;
+	ObSpeed += (-boost) * delta_time;
 //	ObSpeed.y -= 9.8 * delta_time;
-	printf("Y Speed = %f\n", ObSpeed.y);
+	printf("X Speed = %f, Y Speed = %f, Z Speed = %f\n", ObSpeed.x, ObSpeed.y, ObSpeed.z);
 	
 }
